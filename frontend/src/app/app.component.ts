62ac5c68ff0cc5295f1fba534e2c388ebe384d5c
valentinabarbetty <valentinabarbetty2@gmail.com>
2024-09-09 17:16:16 -0500

    front

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component'; 
+import { MainContentComponent } from './main-content/main-content.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, MainContentComponent, ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend/src/app/main-content/main-content.component.ts
+++ frontend/src/app/main-content/main-content.component.ts
@@ -1,12 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './main-content.component.html',
-  styleUrl: './main-content.component.css'
+  styleUrl: './main-content.component.css',
+  
 })
 export class MainContentComponent {
+  selectedAlgorithm: string = '';
+  selectedFile: File | null = null;
 
+  // Método para seleccionar el archivo
+  onFileSelected(event: any): void {
+    this.selectedFile = event.target.files[0];
+    if (this.selectedFile) {
+      console.log(`Archivo seleccionado: ${this.selectedFile.name}`);
+    }
+  }
+
+  // Método para seleccionar el algoritmo
+  selectAlgorithm(algorithm: string): void {
+    this.selectedAlgorithm = algorithm;
+    console.log(`Algoritmo seleccionado: ${this.selectedAlgorithm}`);
+  }
+
+  // Método para moderar la red social
+  moderate(): void {
+    if (this.selectedFile && this.selectedAlgorithm) {
+      console.log(`Moderando con el archivo ${this.selectedFile.name} usando el algoritmo ${this.selectedAlgorithm}`);
+      
+      // Aquí podrías agregar la lógica del análisis con el archivo .txt y el algoritmo seleccionado
+    } else {
+      alert('Por favor, selecciona un archivo y un algoritmo.');
+    }
+  }
 }
