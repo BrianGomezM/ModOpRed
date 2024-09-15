0d8836ed09e1299d81954755318afe8c396215ad
valentinabarbetty <valentinabarbetty2@gmail.com>
2024-09-15 16:34:08 -0500

    cambios en front 2

--- frontend/src/app/main-content/main-content.component.ts
+++ frontend/src/app/main-content/main-content.component.ts
@@ -1,45 +1,61 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import { MatButtonModule } from '@angular/material/button'; // Importar MatButtonModule
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+
 interface Algoritmos {
   value: string;
   viewValue: string;
 }
+
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [CommonModule, MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule, MatButtonModule,  // Agregar MatButtonModule
-    MatIconModule ],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.css',
-  
 })
-
 export class MainContentComponent {
   foods: Algoritmos[] = [
-    {value: 'fb-0', viewValue: 'Fuerza Bruta'},
-    {value: 'pd-1', viewValue: 'Dinámico'},
-    {value: 'pv-2', viewValue: 'Voraz'},
+    { value: 'fb-0', viewValue: 'Fuerza Bruta' },
+    { value: 'pd-1', viewValue: 'Dinámico' },
+    { value: 'pv-2', viewValue: 'Voraz' },
   ];
-  
+
   selectedFile: File | null = null;
+  selectedAlgorithm: string | null = null;
+  isReadyToModerate: boolean = false;
 
   // Método para seleccionar el archivo
   onFileSelected(event: any): void {
     this.selectedFile = event.target.files[0];
-    if (this.selectedFile) {
-      console.log(`Archivo seleccionado: ${this.selectedFile.name}`);
-    }
+    this.checkReadyToModerate();
   }
+
   triggerFileInput() {
     const fileInput = document.getElementById('fileInput') as HTMLInputElement;
     fileInput.click();
   }
 
+  // Método para seleccionar el algoritmo
+  onAlgorithmSelected() {
+    this.checkReadyToModerate();
+  }
 
+  // Verificar si ambos requisitos están cumplidos para habilitar el botón
+  checkReadyToModerate() {
+    this.isReadyToModerate = !!this.selectedFile && !!this.selectedAlgorithm;
+  }
 }
